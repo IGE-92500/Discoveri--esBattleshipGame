f56afb78e2e6db7e84632ab0009ac0588c8f2707
IGE-92500 <nmtvv@iscte-iul.pt>
2026-03-01 21:43:15 +0000

    Update Position.java
    miguel javadoc position2

--- Battleship-main/src/main/java/iscteiul/ista/battleship/Position.java
+++ Battleship-main/src/main/java/iscteiul/ista/battleship/Position.java
@@ -1,18 +1,25 @@
 /**
- *
+ * Implementação concreta de {@link IPosition}.
+ * <p>
+ * Representa uma célula do tabuleiro, identificada por linha e coluna.
+ * Cada posição pode estar ocupada por um navio e pode ser atingida por um disparo.
  */
 package iscteiul.ista.battleship;
 
 import java.util.Objects;
 
 public class Position implements IPosition {
+
     private int row;
     private int column;
     private boolean isOccupied;
     private boolean isHit;
 
     /**
+     * Cria uma nova posição no tabuleiro.
      *
+     * @param row    índice da linha
+     * @param column índice da coluna
      */
     public Position(int row, int column) {
         this.row = row;
@@ -21,102 +28,100 @@ public class Position implements IPosition {
         this.isHit = false;
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see battleship.IPosition#getRow()
+    /**
+     * {@inheritDoc}
      */
     @Override
     public int getRow() {
         return row;
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see battleship.IPosition#getColumn()
+    /**
+     * {@inheritDoc}
      */
     @Override
     public int getColumn() {
         return column;
     }
 
-
+    /**
+     * Gera o código hash baseado nas propriedades da posição.
+     *
+     * @return hash code da posição
+     */
     @Override
     public int hashCode() {
         return Objects.hash(column, isHit, isOccupied, row);
     }
 
-    /*
-     * (non-Javadoc)
+    /**
+     * Compara esta posição com outro objeto.
+     * Duas posições são iguais se tiverem a mesma linha e coluna.
      *
-     * @see battleship.IPosition#equals(java.lang.Object)
+     * @param otherPosition objeto a comparar
+     * @return {@code true} se representam a mesma posição
      */
     @Override
     public boolean equals(Object otherPosition) {
         if (this == otherPosition)
             return true;
+
         if (otherPosition instanceof IPosition) {
             IPosition other = (IPosition) otherPosition;
-            return (this.getRow() == other.getRow() && this.getColumn() == other.getColumn());
-        } else {
-            return false;
+            return (this.getRow() == other.getRow() &&
+                    this.getColumn() == other.getColumn());
         }
+        return false;
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see battleship.IPosition#isAdjacentTo(battleship.IPosition)
+    /**
+     * {@inheritDoc}
      */
     @Override
     public boolean isAdjacentTo(IPosition other) {
-        return (Math.abs(this.getRow() - other.getRow()) <= 1 && Math.abs(this.getColumn() - other.getColumn()) <= 1);
+        return (Math.abs(this.getRow() - other.getRow()) <= 1 &&
+                Math.abs(this.getColumn() - other.getColumn()) <= 1);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see battleship.IPosition#occupy()
+    /**
+     * {@inheritDoc}
      */
     @Override
     public void occupy() {
         isOccupied = true;
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see battleship.IPosition#shoot()
+    /**
+     * {@inheritDoc}
      */
     @Override
     public void shoot() {
         isHit = true;
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see battleship.IPosition#isOccupied()
+    /**
+     * {@inheritDoc}
      */
     @Override
     public boolean isOccupied() {
         return isOccupied;
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see battleship.IPosition#isHit()
+    /**
+     * {@inheritDoc}
      */
     @Override
     public boolean isHit() {
         return isHit;
     }
 
+    /**
+     * Representação textual da posição.
+     *
+     * @return string com linha e coluna
+     */
     @Override
     public String toString() {
-        return ("Linha = " + row + " Coluna = " + column);
+        return "Linha = " + row + " Coluna = " + column;
     }
-
 }
